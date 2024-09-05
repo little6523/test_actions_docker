b731268c5178bab678ff8cd5dffb3cfcac0a1466
hyeonjun.lee <little6523@naver.com>
2024-09-05 11:04:41 +0900

    refactor: 들여쓰기 2칸으로 변경
    들여쓰기 2칸으로 변경

--- src/main/java/com/ormispring/test_actions_docker/TestActionsDockerApplication.java
+++ src/main/java/com/ormispring/test_actions_docker/TestActionsDockerApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestActionsDockerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TestActionsDockerApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(TestActionsDockerApplication.class, args);
+  }
 }
